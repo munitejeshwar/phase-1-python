3dc5fdda7b72c4ff5fae1b013e07d808ae10b852
munitejeshwar <tejeshwark2005@gmail.com>
2025-12-26 20:40:55 +0530

    Day:1 taks completed and updated in notes

--- dsa/1.two-sum.py
+++ dsa/1.two-sum.py
@@ -1,16 +1,54 @@
-def twoSum(nums, target):
-    seen = {}   # value : index
+# def twoSum(nums, target):
+#     seen = {}   # value : index
+
+#     for i in range(len(nums)):
+#         current = nums[i]
+#         needed = target - current
+
+#         if needed in seen:
+#             return [seen[needed], i]
+
+#         seen[current] = i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def twosum(nums,target):
+    seen={}
 
     for i in range(len(nums)):
-        current = nums[i]
-        needed = target - current
+        current=nums[i]
+        needed=target-current
 
         if needed in seen:
-            return [seen[needed], i]
+            return (seen[needed],i)
+        
+        seen[current]=i
 
-        seen[current] = i
 nums = [2, 7, 11, 15]
 target = 9
 
-result = twoSum(nums, target)
+result = twosum(nums, target)
 print(result)
---
+++ notes/day1.md
@@ -0,0 +1,10 @@
+today completed tasks:
+python basics:
+variables,datatypes,input/output,type conversations
+
+learned some git commands,eg; git add . , git commit -m "message"
+but not fully
+
+also learnd brute force and fast approch
+
+solved leetcode:1.twosum
