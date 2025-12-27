e39520c2e9d75841aae7ae199d98985a814d28a4
munitejeshwar <tejeshwark2005@gmail.com>
2025-12-27 11:06:26 +0530

    Day2: Small game added in functions (python-basics)

--- python-basics/functions.py
+++ python-basics/functions.py
@@ -13,7 +13,24 @@
 # s=input()
 # print(reverse(s))
 
-def palindrome(s):
-    return s==s[::-1]
-s=input()
-print(palindrome(s))
+# def palindrome(s):
+#     return s==s[::-1]
+# s=input()
+# print(palindrome(s))
+import random
+
+def play_game():
+    secret=random.randint(1,100)
+    attempts=0
+
+    while True:
+        guess=int(input("guess the number:"))
+        attempts += 1
+
+        if guess>secret:
+            print("Too high")
+        elif guess<secret:
+            print("Too low")
+        else:
+            print("correct the secret number is ",secret,"attempts are",attempts)
+play_game()
