50de3cb7c550fa5d84c21f186f98559e9f8cb702
munitejeshwar <tejeshwark2005@gmail.com>
2026-01-03 11:22:03 +0530

    Day3: Python fundamentals rewritten from scratch

---
+++ python-basics/day3_rewriting_day1,2.py
@@ -0,0 +1,58 @@
+# def evenodd(a):
+#     if a %2==0:
+#         print("Even")
+#     else:
+#         print("Odd")
+# x=int(input())
+# evenodd(x)
+
+# def maxofthree(a,b,c):
+#     if a>b and a>c:
+#         print("A is grater")
+#     elif b>a and b>c:
+#         print("B is greater")
+#     else:
+#         print("C is greater")
+
+# x=int(input("Enter A value: "))
+# y=int(input("Enter B value: "))
+# z=int(input("Enter C value: "))
+# maxofthree(x,y,z)
+
+# def grade(a):
+#     if a>=90:
+#         print("A Grade")
+#     elif a>=80:
+#         print("B Grade")
+#     elif a>=65:
+#         print("C grade")
+#     else: 
+#         print("Fail")
+# x=int(input("Enter marks:"))
+# grade(x)
+
+# def rev(s):
+#     return s[::-1]
+# x=input()
+# print(rev(x))
+
+
+# def pal(s):
+#     return s==s[::-1]
+# s=input()
+# print(pal(s))
+
+
+# import random
+# secret=random.randint(1,10)
+
+
+# while True:
+#     guess= int(input())
+#     if guess==secret:
+#         print("correct")
+#         break
+#     else:
+#         print("try again")
+
+
