2d85936026de9a4629fe0e154dd1522358e47fe5
munitejeshwar <tejeshwark2005@gmail.com>
2026-01-30 18:37:04 +0530

    Day4: Python collections solver some codes

--- python-basics/day4_collections.py
+++ python-basics/day4_collections.py
@@ -1,8 +1,40 @@
-s=input()
-freq={}
-for ch in s:
-    if ch in freq:
-        freq[ch]+=1
-    else:
-        freq[ch]=1
-print(freq)
+# s=input()
+# freq={}
+# for ch in s:
+#     if ch in freq:
+#         freq[ch]+=1
+#     else:
+#         freq[ch]=1
+# print(freq)
+
+
+# nums=list(map(int,input("Enter number by giving space").split()))
+# result=[]
+# seen=set()
+# for n in nums:
+#     if n not in seen:
+#         result.append(n)
+#         seen.add(n)
+# print(result)
+
+
+# while True:
+#     print("\nMenu")
+#     print("1.Add")
+#     print("2.Sub")
+#     print("3.Exit")
+
+#     choice=input()
+#     if choice =="3":
+#         print("Exiting")
+#         break
+
+#     a=int(input("Enter 1st number"))
+#     b=int(input("Enter 2nd number"))
+
+#     if choice=="1":
+#         print(a+b)
+#     elif choice=="2":
+#         print(a-b)
+#     else:
+#         print("Invalid input")
